fe432abd76c23ce77629b87f1592eabbce0a50c6
francaparroz21 <francaparroz21@gmail.com>
2022-12-26 16:23:48 -0300

    We create the recipe controller

---
+++ recycle/src/main/java/com/project/recycle/Controller/RecipeController.java
@@ -0,0 +1,4 @@
+package com.project.recycle.Controller;
+
+public class RecipeController {
+}
--- recycle/src/main/java/com/project/recycle/model/Recipe.java
+++ recycle/src/main/java/com/project/recycle/model/Recipe.java
@@ -1,4 +1,36 @@
 package com.project.recycle.model;
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "recipe")
 public class Recipe {
+
+    @Id
+    @GeneratedValue( strategy= GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String classification;
+    @Column(nullable = false)
+    private String steps;
+
+    public Recipe(String classification, String steps) {
+        this.classification = classification;
+        this.steps = steps;
+    }
+
+    public String getClassification() {
+        return classification;
+    }
+
+    public void setClassification(String classification) {
+        this.classification = classification;
+    }
+
+    public String getSteps() {
+        return steps;
+    }
+
+    public void setSteps(String steps) {
+        this.steps = steps;
+    }
 }
