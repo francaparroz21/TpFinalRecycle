d35bf7b22499601cfd38a8419ca8bf2d65f16e0b
Francisco Caparroz <73504794+francaparroz21@users.noreply.github.com>
2022-12-26 16:01:16 -0300

    Add files via upload

---
+++ recycle/src/main/java/com/project/recycle/RecycleApplication.java
@@ -0,0 +1,13 @@
+package com.project.recycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecycleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecycleApplication.class, args);
+	}
+
+}
---
+++ recycle/src/test/java/com/project/recycle/RecycleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.recycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecycleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
