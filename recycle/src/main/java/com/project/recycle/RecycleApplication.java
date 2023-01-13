4f163e939f476ad5e4e34a04859de232e2c0a4f1
PIL <valentinogiannico04@gmail.com>
2023-01-12 21:08:46 -0300

    delete .idea

--- recycle/src/main/java/com/project/recycle/RecycleApplication.java
+++ recycle/src/main/java/com/project/recycle/RecycleApplication.java
@@ -1,21 +1,6 @@
 package com.project.recycle;
-
-import com.project.recycle.model.Classification;
-import com.project.recycle.model.Report;
-import com.project.recycle.model.Status;
-import com.project.recycle.model.Zone;
-import com.project.recycle.repository.ReportRepository;
-import com.project.recycle.repository.ZoneRepository;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.List;
 
 @SpringBootApplication
 public class RecycleApplication {
