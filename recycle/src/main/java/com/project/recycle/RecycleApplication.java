bb74e6a3efe00285c7d5ce9fee9771f9ac4ea2fd
Valenn4 <94750584+Valenn4@users.noreply.github.com>
2023-01-12 21:19:40 -0300

    Update RecycleApplication.java

--- recycle/src/main/java/com/project/recycle/RecycleApplication.java
+++ recycle/src/main/java/com/project/recycle/RecycleApplication.java
@@ -1,13 +1,12 @@
 package com.project.recycle;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RecycleApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RecycleApplication.class, args);
 	}
+
 
 }
