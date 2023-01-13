4997f19851841d5fe6fe9957619b6d4ac77ce874
Valenn4 <94750584+Valenn4@users.noreply.github.com>
2023-01-13 15:12:47 -0300

    Delete recycle directory

--- recycle/src/main/java/com/project/recycle/repository/ZoneRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.recycle.repository;
-
-import com.project.recycle.model.Zone;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ZoneRepository extends JpaRepository<Zone, Long> {
-}
